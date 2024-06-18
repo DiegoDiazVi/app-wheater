aadeb494b78d12f18acd0f891d9a72440c16e408
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-18 17:45:02 -0500

    ♻️ refactor: refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,17 @@
-import { useEffect, useState } from 'react'
-import { useGetLocation } from './hooks/useGetLocation';
-import { API_KEY, URL_API_WEATHER } from './utils/constants'
+import { useGetWeather } from './hooks/useGetWeather';
+import { isObjectEmpty } from './utils/validateObject';
 import Square from './components/Square'
 import './App.css'
 
 function App() {
-  const [infoWeather, setInfoWeather] = useState([]);
-  const {infoLocation} = useGetLocation();
+  const {infoWeather} = useGetWeather();
 
-  useEffect( () => {
-    const { latitude, longitude} = infoLocation
-    if (!latitude && !latitude) return
-    const queryParams = new URLSearchParams({
-      lat: latitude,
-      lon: longitude,
-      lang: 'es',
-      appid: API_KEY
-    });
-
-    const fetchWeather = async () => {
-      try {
-        const response  = await fetch(`${URL_API_WEATHER}?${queryParams}`)
-        const data = await response.json()
-        console.log(data)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-
-    fetchWeather()
-  }, [infoLocation])
-
-
-
+  console.log(infoWeather)
   return (
     <main className='weather-container'>
-      <Square degrees={18} weather={'Sunnny'} hasIcon/>
-      <Square degrees={18} weather={'Sunnny'} hasIcon/>
-      <Square degrees={18} weather={'Sunnny'} hasIcon/>
+      {!isObjectEmpty(infoWeather) && <Square degrees={infoWeather.temperature} weather={infoWeather.weather} hasIcon/>}
+      {!isObjectEmpty(infoWeather) && <Square degrees={infoWeather.temperature} weather={infoWeather.weather} hasIcon/>}
+      {!isObjectEmpty(infoWeather) && <Square degrees={infoWeather.temperature} weather={infoWeather.weather} hasIcon/>}
     </main>
   )
 }
