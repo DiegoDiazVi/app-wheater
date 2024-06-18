81e3b051acd309c46a7e38c7dc07315b2195e23a
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-17 21:40:34 -0500

    ✨ feat(): add call to weather api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import { useEffect, useState } from 'react'
+import { useGetLocation } from './hooks/useGetLocation';
+import { API_KEY, URL_API_WEATHER } from './utils/constants'
 import Square from './components/Square'
 import './App.css'
-import { useGetLocation } from './hooks/useGetLocation';
 
 function App() {
   const [infoWeather, setInfoWeather] = useState([]);
   const {infoLocation} = useGetLocation();
-  console.log(infoLocation)
+
+  useEffect( () => {
+    const { latitude, longitude} = infoLocation
+    if (!latitude && !latitude) return
+    const queryParams = new URLSearchParams({
+      lat: latitude,
+      lon: longitude,
+      lang: 'es',
+      appid: API_KEY
+    });
+
+    const fetchWeather = async () => {
+      try {
+        const response  = await fetch(`${URL_API_WEATHER}?${queryParams}`)
+        const data = await response.json()
+        console.log(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchWeather()
+  }, [infoLocation])
 
 
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 const API_KEY = 'dd2eb0078e4df75d0b9a3ecd21ae72a0';
 const URL_API_WEATHER = 'https://api.openweathermap.org/data/2.5/weather';
+
+export { API_KEY, URL_API_WEATHER }
