035e906188ff79e661ce5e92aa39c40668f3ec54
DiegoDiazVi <ddiazvi29075@universidadean.edu.co>
2024-06-18 17:42:31 -0500

    ✨ feat: add description prop in Square component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,9 @@
-export default function Square ({degrees, weather, hasIcon}) {
+export default function Square ({degrees, weather, hasIcon, description}) {
     return(
         <section className='square'>
-            {degrees && <h2 className='degrees'>{`${degrees} C`}</h2>}
+            {degrees && <h2 className='degrees'>{`${degrees}° C`}</h2>}
             {weather && <p className='weather'>{weather}</p>}
+            {description && <p className='weather'>{description}</p>}
             {hasIcon && <div className='icon'>Icono</div>}
         </section>
     )
